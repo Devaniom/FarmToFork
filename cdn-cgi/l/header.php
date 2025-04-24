43359454edd69104b7c996ba19ab7ae217ba7cb4
Om Devani <devaniom11@gmail.com>
2025-04-24 20:23:25 +0530

    Add files via upload

---
+++ cdn-cgi/l/header.php
@@ -0,0 +1,16 @@
+<?php include 'db.php'; ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
+    <title>FarmToFork</title>
+
+</head>
+
+<body>
+    <header>
+        <h1>Welcome to FarmToFork</h1>
+    </header>
+</body>
